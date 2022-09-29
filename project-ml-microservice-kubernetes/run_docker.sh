1b34acc826aa6b1e16a073121e84c9cc867200c4
Rajesh Yerramsetty <Rajesh.yerramsetty@pfizer.com>
2022-09-28 21:28:37 -0400

    updates

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Build image
-docker build --tag=api .
+## Complete the following steps to get Docker running locally
 
+# Step 1:
+# Build image and add a descriptive tag
+
+# Step 2: 
 # List docker images
-docker image ls 
 
+# Step 3: 
 # Run flask app
-docker run -p 8000:5001 api
